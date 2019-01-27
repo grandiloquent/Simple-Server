7ee72e9f2b39a093f49feac09b20995f22d0bf65
grandiloquent <1785907865@qq.com>
2019-01-27 21:46:56 +0800

    changed

--- html/test.js
+++
@@ -1,9 +0,0 @@
-function alphabet() {
-    var array = [];
-    for (let n = 0; n < 26; n++) {
-        var s = String.fromCharCode(97 + n)
-        array.push(`${s}=0`);
-    }
-    console.log(array.join(','));
-}
-generate();
